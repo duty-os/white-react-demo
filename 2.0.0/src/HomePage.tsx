dd4df2522d92f9589f18bec6f65db49587f4368f
Moskize91 <i@taozeyu.com>
2019-02-13 15:29:11 +0800

    add router

---
+++ 2.0.0/src/HomePage.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+
+export class HomePage extends React.Component {
+
+    public render(): React.ReactNode {
+        return null;
+    }
+}
--- 2.0.0/src/index.tsx
+++ 2.0.0/src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
 
+import {App} from './App';
 import "white-web-sdk/style/index.css";
 
 import registerServiceWorker from './registerServiceWorker';
